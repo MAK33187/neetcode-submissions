423fb7fbeaace2499beb4a50311a749a120f9648
Muhammad Ali Khann <239726795+MAK33187@users.noreply.github.com>
2026-09-04 16:38:48 +0500

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> a;
+        unordered_map<char, int> b;
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(char i : s){
+            a[i]++;
+        }
+        for(char j : t){
+            b[j]++;
+        }
+        if(a==b){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+
+    }
+};
